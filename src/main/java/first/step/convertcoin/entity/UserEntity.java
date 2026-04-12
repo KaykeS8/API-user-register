7674aee7044953fe7b383fd418d9174c8036105a
Kayke simao <kayke.py@gmail.com>
2026-04-12 15:10:47 -0300

    Change long (primitve type) to Long (wrapper class) for not happen nullpointerexception

--- src/main/java/first/step/convertcoin/entity/UserEntity.java
+++ src/main/java/first/step/convertcoin/entity/UserEntity.java
@@ -1,9 +1,6 @@
 package first.step.convertcoin.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -42,11 +39,11 @@ public class UserEntity {
         this.name = name;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 }
